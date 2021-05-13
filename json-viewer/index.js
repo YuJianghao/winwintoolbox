284d4d0a851d2dc30d6fb8d81db53210e06acb3f
winwin_2011 <yjh2011@live.com>
2021-05-13 16:50:46 +0800

    feat: 支持多语言格格式化

--- json-viewer/index.js
+++ json-viewer/index.js
@@ -6,11 +6,33 @@ require.config({
 });
 
 require(["vs/editor/editor.main"], function () {
+  let current = 'json'
+  const languages = ['json', 'javascript', 'html']
+  const buttonContainer = document.getElementById('buttons')
+  const buttons = languages.reduce((o, id) => {
+    const button = document.createElement('button')
+    button.setAttribute('id', id)
+    button.innerText = `Format ${id}`
+    buttonContainer.appendChild(button)
+    o[id] = button
+    return o
+  }, {})
   const editor = monaco.editor.create(container, {
     value: `{\n    "key": "value"\n}`,
     theme: "vs-dark",
-    language: "json",
+    language: current
   });
+  function bind(node, language) {
+    node.addEventListener('click', () => {
+      if (current !== language) {
+        current = language
+        monaco.editor.setModelLanguage(editor.getModel(), language);
+      }
+      editor.trigger('anyString', 'editor.action.formatDocument')
+      console.log('Format', language)
+    })
+  }
+  Object.entries(buttons).forEach(([language, node]) => bind(node, language))
   window.addEventListener("resize", () => {
     editor.layout();
   });
